bd64fc80e5ab1c00f472bd104d54da56021614c9
Sadik Habibulla <iam.mrsadik01@gmail.com>
2025-08-06 14:19:13 +0530

    Update app.py
    added
    if __name__ == "__main__":
        port = int(os.environ.get("PORT", 5000))
        app.run(debug=False, host="0.0.0.0", port=port)
    on 254 line

--- app.py
+++ app.py
@@ -250,8 +250,12 @@ Supporting Routes and Error Handler
 '''
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+#if __name__ == '__main__':
+ #   app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
 '''
 Main entry point of the Flask application.
 
@@ -261,4 +265,5 @@ Main entry point of the Flask application.
 
 Note: Set `debug=False` when deploying to production.
 '''
+
 # ========== End of Flask App ==========
