21637d93057cf5a879af7d83ec3f73c6cee36a61
MrSadik-01 <iam.mrsadik01@gmail.com>
2025-08-07 17:41:04 +0530

    edited else block

--- app.py
+++ app.py
@@ -263,6 +263,7 @@ if __name__ == '__main__':
     if os.getenv('FLASK_ENV') == 'production':
         app.run(host='0.0.0.0', port=int(os.getenv('PORT', 10000)))
     else:
+        app.run(debug=True, port=5000)
 '''
 Main entry point of the Flask application.
 
